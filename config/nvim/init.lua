f5d4cf7261a02e9f10eb252433c6b17e5ab74725
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2025-03-02 00:20:02 -0600

    Quit special bufs on 'q'

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -55,3 +55,38 @@ require("lazy").setup({
     { import = 'config.plugins.colorscheme' },
   },
 })
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.create_augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "checkhealth",
+    "dbout",
+    "gitsigns-blame",
+    "grug-far",
+    "help",
+    "lspinfo",
+    "neotest-output",
+    "neotest-output-panel",
+    "neotest-summary",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
+  end,
+})
