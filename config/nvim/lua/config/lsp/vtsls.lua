b40d312e147b440f8195bf3412b94e906c860e2c
Benoit Barberousse <74591246+bnwa@users.noreply.github.com>
2025-03-06 01:06:51 -0600

    Update VTSLS config

--- config/nvim/lua/config/lsp/vtsls.lua
+++ config/nvim/lua/config/lsp/vtsls.lua
@@ -1,89 +1,157 @@
--- General LSP client so no runtime fields
-return {
-  ---@type vim.lsp.ClientConfig
-  ---@diagnostic disable-next-line:missing-fields
-  vtsls = {
-    settings = {
-      ['typescript.disableAutomaticTypeAcquisition'] = true,
-      ['typescript.check.npmIsInstalled'] = false,
-
-      ['javascript.referencesCodeLens.enabled'] = true,
-      ['javascript.referencesCodeLens.showOnAllFunctions'] = true,
-
-      ['typescript.referencesCodeLens.enabled'] = true,
-      ['typescript.referencesCodeLens.showOnAllFunctions'] = true,
-
-      ['typescript.implementationsCodeLens.enabled'] = true,
-      ['typescript.implementationsCodeLens.showOnInterfaceMethods'] = true,
-
-      ['typescript.tsserver.enableTracing'] = false,
-      ['typescript.tsserver.log'] = 'off',
-
-      ['javascript.suggest.completeFunctionCalls'] = true,
-      ['javascript.suggest.includeAutomaticOptionalChainCompletions'] = true,
-
-      ['typescript.suggest.completeFunctionCalls'] = true,
-      ['typescript.suggest.includeAutomaticOptionalChainCompletions'] = true,
-
-      ['typescript.inlayHints.parameterNames.enabled'] = 'all',
-      ['typescript.inlayHints.parameterNames.suppressWhenArgumentMatchesName'] = true,
-      ['typescript.inlayHints.parameterTypes.enabled'] = false,
-      ['typescript.inlayHints.variableTypes.enabled'] = false,
-      ['typescript.inlayHints.variableTypes.suppressWhenTypeMatchesName'] = true,
-      ['typescript.inlayHints.propertyDeclarationTypes.enabled'] = false,
-      ['typescript.inlayHints.functionLikeReturnTypes.enabled'] = false,
-      ['typescript.inlayHints.enumMemberValues.enabled'] = false,
-
-      ['javascript.inlayHints.parameterNames.enabled'] = 'all',
-      ['javascript.inlayHints.parameterNames.suppressWhenArgumentMatchesName'] = true,
-      ['javascript.inlayHints.parameterTypes.enabled'] = false,
-      ['javascript.inlayHints.variableTypes.enabled'] = false,
-      ['javascript.inlayHints.variableTypes.suppressWhenTypeMatchesName'] = true,
-      ['javascript.inlayHints.propertyDeclarationTypes.enabled'] = false,
-      ['javascript.inlayHints.functionLikeReturnTypes.enabled'] = false,
-      ['javascript.inlayHints.enumMemberValues.enabled'] = false,
-
-      ['javascript.suggest.includeCompletionsForImportStatements'] = true,
-      ['typescript.suggest.includeCompletionsForImportStatements'] = true,
-
-      ['typescript.reportStyleChecksAsWarnings'] = true,
-
-      ['typescript.validate.enable'] = true,
-
-      ['typescript.format.enable'] = true,
-      ['typescript.format.insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets'] = true,
-      ['typescript.format.semicolons'] = 'remove',
-
-      ['javascript.format.enable'] = true,
-      ['javascript.format.insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets'] = true,
-      ['javascript.format.semicolons'] = 'remove',
-
-      ['javascript.preferences.useAliasesForRenames'] = false,
-      ['typescript.preferences.useAliasesForRenames'] = false,
-
-      ['typescript.updateImportsOnFileMove.enabled'] = 'always',
-      ['javascript.updateImportsOnFileMove.enabled'] = 'always',
-
-      ['typescript.tsserver.experimental.enableProjectDiagnostics'] = true,
-
-      ['vtsls.javascript.format.indentSize'] = 2,
-      ['vtsls.javascript.format.convertTabsToSpaces'] = true,
-      ['vtsls.javascript.format.trimTrailingWhitespace'] = true,
-      ['vtsls.javascript.format.indentStyle'] = 'smart',
-
-      ['vtsls.typescript.format.indentSize'] = 2,
-      ['vtsls.typescript.format.convertTabsToSpaces'] = true,
-      ['vtsls.typescript.format.trimTrailingWhitespace'] = true,
-      ['vtsls.typescript.format.indentStyle'] = 'smart',
-
-      ['vtsls.experimental.completion.enableServerSideFuzzyMatch'] = true,
-
-      -- TODO requires handler
-      ['vtsls.enableMoveToFileCodeAction'] = true,
+local shared = {
+  check = {
+    npmIsInstalled = false,
+  },
+  disableAutomaticTypeAcquisition = true,
+  format = {
+    enabled = true,
+    indentSwitchCase = true,
+    insertSpaceAfterCommaDelimiter = true,
+    insertSpaceAfterConstructor = false,
+    insertSpaceAfterFunctionKeywordForAnonymousFunctions = true,
+    insertSpaceAfterKeywordsInControlFlowStatements = true,
+    insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = true,
+    insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces = false,
+    insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = true,
+    insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets = true,
+    insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis = false,
+    insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces = false,
+    insertSpaceAfterSemicolonInForStatements = true,
+    insertSpaceAfterTypeAssertion = true,
+    insertSpaceBeforeAndAfterBinaryOperators = true,
+    insertSpaceBeforeFunctionParenthesis = false,
+    insertSpaceBeforeTypeAnnotation = true,
+    organizeImportsIgnoreCase = false,
+    organizeImportsCaseFirst = true,
+    placeOpenBraceOnNewLineForControlBlocks = false,
+    placeOpenBraceOnNewLineForFunctions = false,
+    semicolons = 'remove',
+  },
+  inlayHints = {
+    enabled = false,
+    includeInlayEnumMemberValueHints = true,
+    includeInlayFunctionLikeReturnTypeHints = true,
+    includeInlayFunctionParameterTypeHints = true,
+    includeInlayParameterNameHints = 'all',
+    includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+    includeInlayVariableTypeHints = true,
+    includeInlayPropertyDeclarationTypeHints = true,
+    enumMemberValues = {
+      enabled = true,
     },
-    setup = function()
+    functionLikeReturnTypes = {
+      enabled = true,
+    },
+    parameterNames = {
+      enabled = "all",
+      suppressWhenArgumentMatchesName = true,
+    },
+    parameterTypes = {
+      enabled = true,
+    },
+    propertyDeclarationTypes = {
+      enabled = true,
+    },
+    variableTypes = {
+      enabled = true,
+      suppressWhenTypeMatchesName = true,
+    },
+  },
+  implementationsCodeLens = {
+    enabled = true,
+    showOnInterfaceMethods = true,
+  },
+  preferGoToSourceDefinition = false,
+  preferences = {
+    importModuleSpecifier = 'shortest',
+    importModuleSpecifierEnding = 'auto',
+    includePackageJsonAutoImports = 'on',
+    jsxAttributeCompletionStyle = 'auto',
+    preferTypeOnlyAutoImports = true,
+    quoteStyle = 'auto',
+    renameMatchingJsxTags = true,
+    useAliasesForRenames = false,
+  },
+  referencesCodeLens = {
+    enabled = true,
+    showOnAllFunctions = true,
+  },
+  reportStyleChecksAsWarnings = true,
+  suggest = {
+    autoImports = true,
+    classMemberSnippets = {
+      enabled = true,
+    },
+    completeFunctionCalls = false,
+    completeJSDocs = true,
+    enabled = true,
+    includeAutomaticOptionalChainCompletions = true,
+    includeCompletionsForImportStatements = true,
+    jsdoc = {
+      generateReturns = true,
+    },
+    objectLiteralMethodSnippets = {
+      enabled = true,
+    },
+    paths = true,
+  },
+  suggestionActions = {
+    enabled = true,
+  },
+  updateImportsOnFileMove = {
+    enabled = 'always',
+  },
+  validate = {
+    enabled = true,
+  },
+}
+
+local vtsls_format = {
+  format = {
+    baseIndentSize = 2,
+    convertTabsToSpaces = true,
+    indentSize = 2,
+    indentStyle = 2,
+    trimTrailingWhitespace = true,
+  },
+}
+
+local vtsls = {
+  enableMoveToFileCodeAction = true,
+  experimental = {
+    completion = {
+      enableServerSideFuzzyMatch = true,
+    },
+  },
+  javascript = vtsls_format,
+  typescript = vtsls_format,
+}
+
+local ts = {
+  tsserver = {
+    experimental = {
+      enabledProjectDiagnostics = true,
+    },
+    maxTsServerMemory = 8092,
+    useSyntaxServer = 'auto',
+    workspaceSymbols = {
+      scope = 'allOpenProjects', -- allOpenProjects | currentProject
+      excludeLibrarySymbols = true,
+    },
+  },
+}
+
+return {
+  vtsls = {
+    setup = function(config)
+      local lsp = require 'lspconfig'
       require('lspconfig.configs').vtsls = require('vtsls').lspconfig
-      return true
-    end
-  }
+      lsp.vtsls.setup(config)
+    end,
+    settings = {
+      javascript = shared,
+      typescript = vim.tbl_extend('force', {}, ts, shared),
+      vtsls = vtsls,
+    },
+  },
 }
